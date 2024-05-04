685b81a8a879f188b4924f792af7ea5a3e5bc1ef
CJ-ONYERO99 <67972505+CJ-ONYERO99@users.noreply.github.com>
2024-05-04 18:33:29 +0100

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["52.91.121.146", "3.85.136.181"]
+env.hosts = ["18.235.255.90", "34.224.16.161"]
 
 
 def do_pack():
